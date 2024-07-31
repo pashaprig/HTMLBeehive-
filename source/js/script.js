fa229cf52bc161e11b0aa56ce3f7b9f316f3f02c
Pavlo Pryharnytskyi <pashaprig@gmail.com>
2024-07-31 22:11:53 +0300

    Card desktop

--- source/js/script.js
+++ source/js/script.js
@@ -1,51 +1,10 @@
 class App {
   init() {
-    this.initMobileMenu();
   }
 
   constructor() {
   }
 
-  initMobileMenu() {
-    const navMain = document.querySelector('.main-nav');
-    const navToggle = document.querySelector('.main-nav__toggle');
-    const navButtonText = document.querySelector('.main-nav__open-btn-text');
-
-    const initJS = () => {
-      navMain.classList.remove('main-nav--nojs');
-    }
-
-    const closeOpen = () => {
-      navToggle.addEventListener('click', function () {
-        if (navMain.classList.contains('main-nav--closed')) {
-          navMain.classList.remove('main-nav--closed');
-          navMain.classList.add('main-nav--opened');
-          navButtonText.classList.add('visually-hidden');
-        } else {
-          navMain.classList.add('main-nav--closed');
-          navMain.classList.remove('main-nav--opened');
-        }
-      });
-    }
-
-    const linksClick = () => {
-      const mainNav = document.querySelector('.main-nav');
-      const links = mainNav.querySelectorAll('a');
-
-      const navLinckHandleClick = () => {
-        navMain.classList.add('main-nav--closed');
-        navMain.classList.remove('main-nav--opened');
-      }
-
-      links.forEach(link => {
-        link.addEventListener('click', navLinckHandleClick)
-      })
-    }
-
-    initJS();
-    closeOpen();
-    linksClick();
-  }
 }
 
 const app = new App();
